02ae71e24fdfd3e2e92449ac7cf3fd2027e0c69b
ownax-vit <ownax@mail.ru>
2024-09-10 17:20:24 +0300

    fix: checking exists title in repo

--- app/tests/logic/test_messages.py
+++ app/tests/logic/test_messages.py
@@ -1,22 +1,40 @@
 import pytest
 
+from faker import Faker
 
 from domain.entities.messages import Chat
+from domain.values.messages import Title
 from infra.repositories.messages import BaseChatRepository
 from logic.commands.messages import CreateChatCommand
+from logic.exceptions.messages import ChatWithThatTitleAlreadyExistsException
 from logic.mediator import Mediator
 
 
 @pytest.mark.asyncio
 async def test_create_chat_command_success(
-    chat_repository: BaseChatRepository,
-    mediator: Mediator,
+    chat_repository: BaseChatRepository, mediator: Mediator, faker: Faker
 ):
+    title = Title(faker.text())
     chat: Chat
     chat, *_ = await mediator.handle_command(
-        CreateChatCommand(title="megaTitle"),
+        CreateChatCommand(title=title.as_generic_type()),
     )
 
-    assert chat_repository.check_chat_exists_by_title(
+    assert await chat_repository.check_chat_exists_by_title(
         title=chat.title.as_generic_type()
     )
+
+
+@pytest.mark.asyncio
+async def test_chat_command_title_exists(
+    chat_repository: BaseChatRepository, mediator: Mediator, faker: Faker
+):
+    title_text = faker.text()
+    chat = Chat(title=Title(title_text))
+
+    await chat_repository.add_chat(chat=chat)
+
+    with pytest.raises(ChatWithThatTitleAlreadyExistsException):
+        chat, *_ = await mediator.handle_command(
+            CreateChatCommand(title=title_text),
+        )
